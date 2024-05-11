4d3469ed73d8456249c0790df33f2cc1cace7222
HUSEYIN OZDEMIR <78041148+HsynHsyn@users.noreply.github.com>
2024-05-11 10:10:50 +0100

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,21 @@
+package Day4_Arithmetic_Operators;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				double perimeter, area, radius;
+				final double Pi =3.14;
+				 radius= 5.5;
+				
+				 
+				
+				perimeter = 2 * radius * Pi;
+				area = radius * radius * Pi;
+				
+				System.out.println("Perimeter of the cycle is " + perimeter + "\n" + "Area of the cycle is " + area);
+		
+	}
+ 
+}
